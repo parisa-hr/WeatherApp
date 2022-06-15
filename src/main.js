eb7702df77b869235c7495f8cbde6e03a28bfeb5
parisa <parisahamed76@gmail.com>
2022-06-15 15:09:47 +0430

    update

--- src/main.js
+++ src/main.js
@@ -21,41 +21,19 @@ $(document).ready(function() {
   
     function init() {
   
-      //stworzenie konteneru na canve
       container = document.getElementById("clouds");
   
-      //stworzenie canvy
       var canvas = document.createElement("canvas");
       canvas.width = 32;
       canvas.height = window.innerHeight;
   
-      /* 
-          Początkowo płótno jest puste, pozbawione tła. 
-          Aby coś na nim wyświetlić, skrypt musi posiadać dostęp do 
-          kontekstu renderowania a następnie musi coś w nim narysować. 
-      */
       var context = canvas.getContext("2d");
   
-      // Stworzenie gradientu liniowego
       var gradient = context.createLinearGradient(0, 0, 0, canvas.height);
   
-      // Wypelnij kontekst renderowania gradientem
       context.fillStyle = gradient;
       context.fillRect(0, 0, canvas.width, canvas.height);
-  
-      // Stylizacja tła
-      // container.style.backgroundColor = "0x4584b4";
-      // container.style.backgroundSize = "32px 100%";
-  
-    // Umiejscowienie kamery
-    
-    /**
-     * PerspectiveCamera( fov : Number, aspect : Number, near : Number, far : Number )
-          fov — Camera frustum vertical field of view.
-          aspect — Camera frustum aspect ratio.
-          near — Camera frustum near plane.
-          far — Camera frustum far plane.
-     */
+ 
       camera = new THREE.PerspectiveCamera(
         50,
         window.innerWidth / window.innerHeight,
@@ -64,23 +42,18 @@ $(document).ready(function() {
       );
       camera.position.z = 6000;
   
-      // Stworzenie sceny
       scene = new THREE.Scene();
   
-      // Stworzenie geometrii
       geometry = new THREE.Geometry();
   
-      // Załadowanie tekstury (chmurka)
       var texture = THREE.ImageUtils.loadTexture(
-        "https://i.ibb.co/L6hRx8h/cloud1.png",
+        "../assets/images/cloud.png",
         null,
         animate
       );
   
-      //Tworzenie mgly
       var fog = new THREE.Fog(0x4584b4, -100, 3000);
   
-      //tworzenie ShaderMaterial
       material = new THREE.ShaderMaterial({
         uniforms: {
           map: { type: "t", value: texture },
@@ -95,10 +68,8 @@ $(document).ready(function() {
         transparent: true
       });
   
-      //stworzenie plaszczyzny, na ktorej bedzie rysowana chmura
       var plane = new THREE.Mesh(new THREE.PlaneGeometry(65, 65));
   
-      // stworz plaszczyzny z chmurkami, w randomowych miejscach i z randomowym obrotem
       for (var i = 0; i < 8000; i++) {
         plane.position.x = Math.random() * 1000 - 500;
         plane.position.y = -(Math.floor(Math.random() * (90 - 15)) - 15);
@@ -110,7 +81,6 @@ $(document).ready(function() {
         THREE.GeometryUtils.merge(geometry, plane);
       }
   
-    //stworz siatke
       mesh = new THREE.Mesh(geometry, material);
       scene.add(mesh);
   
@@ -118,7 +88,6 @@ $(document).ready(function() {
       mesh.position.z = -8000;
       scene.add(mesh);
   
-      //renserer, ktory wyrenderuje scene
       renderer = new THREE.WebGLRenderer({ antialias: false });
       renderer.setSize(window.innerWidth, window.innerHeight);
       container.appendChild(renderer.domElement);
@@ -127,20 +96,16 @@ $(document).ready(function() {
       window.addEventListener("resize", onWindowResize, false);
     }
   
-    // funkcja reagujaca na zmiany polozenia kursora myszy
-    // zmienia wskazania miernikow oraz polozenie kamery
     function onDocumentMouseMove(event) {
       mouseX = (event.clientX - windowHalfX) * 0.25;
       mouseY = (event.clientY - windowHalfY) * 0.15
       mouseX= Math.round(mouseX * 100) / 100
       mouseY= Math.round(mouseY * 100) / 100
       
-      //wyswietl wartosci na licznikach
       $('#measurement-mouseX-value').text(mouseX);
       $('#measurement-mouseY-value').text(mouseY);
     }
   
-    //reakcja na zmiane rozmiaru okna
     function onWindowResize(event) {
       camera.aspect = window.innerWidth / window.innerHeight;
       camera.updateProjectionMatrix();
@@ -148,7 +113,6 @@ $(document).ready(function() {
       renderer.setSize(window.innerWidth, window.innerHeight);
     }
   
-    // animacja
     function animate() {
       requestAnimationFrame(animate);
   
